8800a0661eb5a8c1da6ee3db7fa6289f828a6cc9
tischrei <tino.schreiber@t-systems.com>
2024-04-19 13:32:00 +0000

    deployment changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: '/gh-actions',
+    assetPrefix: '/gh-actions/',
 };
 
 export default nextConfig;
